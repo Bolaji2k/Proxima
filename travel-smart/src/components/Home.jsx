cbcd5958d6c688695f2be972b30da1b16dfb3302
Bolaji2k <khashkong@gmail.com>
2024-09-20 11:48:18 +0100

    Create: Home

---
+++ travel-smart/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import Contact from "./layout/Contact";
+import About from "./layout/About";
+import Explore from "./layout/Explore"
+const Home = () => {
+    return ( 
+        <> 
+        <Explore/>
+        <About />
+        <Contact />
+        </>
+     );
+}
+ 
+export default Home;
---
+++ travel-smart/src/components/layout/Header.jsx
@@ -0,0 +1,70 @@
+import { useEffect } from "react";
+import { memo } from "react";
+
+const Header = () => {
+  const search = () => {
+    // Define your search function logic here
+    console.log("Search button clicked");
+  };
+
+  useEffect(() => {
+    const header = document.querySelector("header");
+    const logotext = document.querySelector(".header h1 ");
+    const logo = document.querySelector(".logo");
+
+    window.addEventListener("scroll", () => {
+      header.classList.toggle("sticky", window.scrollY > 0);
+      logotext.classList.toggle("primary-color", window.scrollY > 0);
+      logo.classList.toggle("alternate-logo", window.scrollY > 0);
+    });
+  }, []);
+
+  return (
+    <section className="hero-header">
+      <header>
+        <div className="container">
+          <div className="header">
+            <h1>
+              <i className="logo"></i>
+              TravelSmart
+            </h1>
+            <nav>
+              <ul>
+                <li>
+                  <a href="#home">Home</a>
+                </li>
+                <li>
+                  <a href="#tours">Tours</a>
+                </li>
+                <li>
+                  <a href="#about">About</a>
+                </li>
+                <li>
+                  <a href="#contact">Contact</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      <section id="hero">
+        <div className="container">
+          <h2>Discover Your Next Adventure</h2>
+          <h3>
+            Explore the best tours, destinations, and activities worldwide
+          </h3>
+          <div>
+            <a href="#explore">
+              <button id="hero-button" type="button" onClick={search}>
+                Explore
+              </button>
+            </a>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default memo(Header);
